cedfae1613ac76c6e279b71b6f3af4e2f6a569a0
Jacger <quangjacger99@gmail.com>
2022-09-17 15:40:37 +0700

    user reducer

--- web/src/contexts/user.context.jsx
+++ web/src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useReducer } from "react";
 import Cookies from "universal-cookie";
 
 export const UserContext = createContext({
@@ -7,16 +7,40 @@ export const UserContext = createContext({
   saveUserInfo: () => {},
 });
 
+const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+const INITIAL_STATE = {
+  currentUser: new Cookies().get("user"),
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
 export const UserProvider = ({ children }) => {
-  const cookies = new Cookies();
-  const [currentUser, setCurrentUser] = useState(cookies.get("user"));
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+  }
   const value = { currentUser, setCurrentUser, saveUserInfo };
   console.log("UserProvider");
 
   function saveUserInfo(user) {
     new Cookies().set("user", user, { path: "/", sameSite: true });
     setCurrentUser(user);
-  };
+  }
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
